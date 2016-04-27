3945961051f074875454094e23dac5ec0d9af630
zhoujunqian <zjqzero@163.com>
2016-04-27 23:10:14 +0800

    [zhoujunqian] add index

---
+++ add_index/test.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/VWMS?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+
+if __name__ == '__main__':
+    manager.run()
